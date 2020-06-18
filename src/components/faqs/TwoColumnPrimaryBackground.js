55427bd0fe6fe17ae271f6e67c49d9f166870591
Kellen Presta <kpresta04@gmail.com>
2020-06-18 13:49:28 -0500

    faq page

--- src/components/faqs/TwoColumnPrimaryBackground.js
+++ src/components/faqs/TwoColumnPrimaryBackground.js
@@ -1,77 +1,83 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import tw from "twin.macro";
-import styled, { css } from "styled-components/macro"; //eslint-disable-line
-import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
-import { SectionDescription } from "components/misc/Typography.js";
-import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
+import React, { useState } from "react"
+import { motion } from "framer-motion"
+import tw from "twin.macro"
+import styled, { css } from "styled-components/macro" //eslint-disable-line
+import { Container, ContentWithPaddingXl } from "../misc/Layouts.js"
+import {
+  SectionHeading,
+  Subheading as SubheadingBase,
+} from "../misc/Headings.js"
+import { SectionDescription } from "../misc/Typography.js"
+import ChevronDownIcon from "../../images/chevron-down.svg"
 
-const PrimaryBackgroundContainer = tw(Container)`-mx-8 px-8 bg-primary-900 text-gray-100`;
+const PrimaryBackgroundContainer = tw(
+  Container
+)`-mx-8 px-8 bg-white text-blue-900`
 
-const HeadingContainer = tw.div``;
-const Subheading = tw(SubheadingBase)`text-center text-gray-100 mb-4`;
-const Heading = tw(SectionHeading)``;
-const Description = tw(SectionDescription)`mx-auto text-center text-gray-300`;
+const HeadingContainer = tw.div``
+const Subheading = tw(SubheadingBase)`text-center text-gray-100 mb-4`
+const Heading = tw(SectionHeading)``
+const Description = tw(SectionDescription)`mx-auto text-center text-gray-300`
 
-const FaqsContainer = tw.div`mt-10 sm:mt-16 w-full flex-1 lg:flex justify-between items-start max-w-screen-lg mx-auto`;
-const FaqsColumn = tw.div`w-full lg:max-w-lg lg:mr-12 last:mr-0`;
-const Faq = tw.div`select-none cursor-pointer border-b-2 border-primary-300 hover:border-primary-500 transition-colors duration-300 py-6`;
-const Question = tw.div`flex justify-between items-center`;
-const QuestionText = tw.div`text-sm sm:text-lg font-semibold tracking-wide`;
+const FaqsContainer = tw.div`mt-10 sm:mt-16 w-full flex-1 lg:flex justify-between items-start max-w-screen-lg mx-auto`
+const FaqsColumn = tw.div`w-full lg:max-w-lg lg:mr-12 last:mr-0`
+const Faq = tw.div`select-none cursor-pointer border-b-2 border-blue-700 hover:border-blue-500 transition-colors duration-300 py-6`
+const Question = tw.div`flex justify-between items-center`
+const QuestionText = tw.div`text-sm sm:text-lg font-semibold tracking-wide`
 const QuestionToggleIcon = styled(motion.span)`
   ${tw`ml-2 transition duration-300`}
   svg {
     ${tw`w-6 h-6`}
   }
-`;
-const Answer = tw(motion.div)`hidden text-sm font-normal mt-4 text-gray-300`;
+`
+const Answer = tw(motion.div)`hidden text-sm font-normal mt-4 text-blue-900`
 
 export default ({
   subheading = "",
   heading = "Frequently Asked Questions",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  description = "",
   faqs = [
     {
-      question: "Is lunch provided free of cost ?",
+      question:
+        "What are the delivery charges for orders from the Online Shop?",
       answer:
-        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system."
+        "We will ship the firearm container and box to you free of charge AND pay the return freight. You only pay the freight to receive the repaired firearm after finished. All other products or services are priced based on weight, length and distance of shipping.",
     },
     {
-      question: "Do you have 2 Bedroom suites ?",
+      question: "Which payment methods are accepted in the Online Shop?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "We accept major credit cards and we can accept PayPal purchases as well.",
     },
     {
-      question: "Are Wi-Fi costs included in the price ?",
+      question: "How long will delivery take?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "Firearm purchases will take less than 1 week. Repairs will be quoted on a case by case basis.",
     },
     {
-      question: "Where can I reach you for support ?",
+      question: "How secure is shopping in the Online Shop? Is my data secure?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "All data is encrypted and secure. Our site is state of the art and you can have true peace of mind when shopping online with us.",
     },
     {
-      question: "What kind of SLA Guarantee do you provide ? ",
+      question: "What exactly happens after ordering? ",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "Firearm purchases are immediately pulled from our warehouse and shipped directly to your FFL in the town you are located in. For gun repairs, shipping containers are sent directly to you with a prepaid label for you to ship them to us. When your repair is finished, we will ship your firearm directly back to you in the container you bought.",
     },
     {
-      question: "Where are the servers located ?",
+      question: "Do I receive an invoice for my order?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    }
-  ]
+        "Yes. You will receive an online invoice at the time of purchase, and another copy will arrive with your firearm or related purchase.",
+    },
+  ],
 }) => {
-  const faqCol1 = [];
-  const faqCol2 = [];
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
+  const faqCol1 = []
+  const faqCol2 = []
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null)
 
   const toggleQuestion = questionIndex => {
-    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
-    else setActiveQuestionIndex(questionIndex);
-  };
+    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null)
+    else setActiveQuestionIndex(questionIndex)
+  }
 
   faqs.map((faq, index) => {
     const renderedFaq = (
@@ -81,7 +87,7 @@ export default ({
           <QuestionToggleIcon
             variants={{
               collapsed: { rotate: 0 },
-              open: { rotate: -180 }
+              open: { rotate: -180 },
             }}
             initial="collapsed"
             animate={activeQuestionIndex === index ? "open" : "collapsed"}
@@ -92,8 +98,18 @@ export default ({
         </Question>
         <Answer
           variants={{
-            open: { opacity: 1, height: "auto", marginTop: "16px", display: "block" },
-            collapsed: { opacity: 0, height: 0, marginTop: "0px", display: "none" }
+            open: {
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px",
+              display: "block",
+            },
+            collapsed: {
+              opacity: 0,
+              height: 0,
+              marginTop: "0px",
+              display: "none",
+            },
           }}
           initial="collapsed"
           animate={activeQuestionIndex === index ? "open" : "collapsed"}
@@ -102,13 +118,13 @@ export default ({
           {faq.answer}
         </Answer>
       </Faq>
-    );
+    )
 
-    if (index % 2 === 0) faqCol1.push(renderedFaq);
-    else faqCol2.push(renderedFaq);
+    if (index % 2 === 0) faqCol1.push(renderedFaq)
+    else faqCol2.push(renderedFaq)
 
-    return null;
-  });
+    return null
+  })
   return (
     <PrimaryBackgroundContainer>
       <ContentWithPaddingXl>
@@ -123,5 +139,5 @@ export default ({
         </FaqsContainer>
       </ContentWithPaddingXl>
     </PrimaryBackgroundContainer>
-  );
-};
+  )
+}
---
+++ src/pages/faq.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/Layout/Layout"
+import TwoColumnPrimaryBackground from "../components/faqs/TwoColumnPrimaryBackground"
+
+export default function Faq() {
+  return (
+    <Layout>
+      <TwoColumnPrimaryBackground />
+    </Layout>
+  )
+}

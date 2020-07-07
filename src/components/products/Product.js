484949b68b09aa9748c727dcece6859e9d9ed8c6
Kellen Presta <kpresta04@gmail.com>
2020-07-07 14:13:19 -0500

    7

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,13 +1,17 @@
-import React from "react"
-
+import React, { useContext } from "react"
+import StoreContext from "~/context/StoreContext"
 import Layout from "../Layout/Layout"
 
 const ProductTemplate = ({ pageContext }) => {
   const { product } = pageContext
+  const { addProductToCart } = useContext(StoreContext)
   console.log(product)
   return (
     <Layout>
       <div style={{ minHeight: "50vh" }}>
+        <button onClick={() => addProductToCart(product.variants[0].shopifyId)}>
+          Add To Cart
+        </button>
         <h1>{product.title}</h1>
         <div>{product.description}</div>
       </div>
--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Client from "shopify-buy"
 
 const client = Client.buildClient({
@@ -15,21 +15,33 @@ const defaultContext = {
 const StoreContext = React.createContext(defaultContext)
 
 export const StoreProvider = ({ children }) => {
-  const addProductToCart = async variantId => {
+  const [checkoutId, setCheckoutId] = useState({})
+
+  const initializeCheckout = async () => {
     try {
       const newCheckout = await client.checkout.create()
+      setCheckoutId(newCheckout.id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    initializeCheckout()
+  }, [])
+
+  const addProductToCart = async variantId => {
+    try {
+      // const newCheckout = await client.checkout.create()
       const lineItems = [
         {
           variantId,
           quantity: 1,
         },
       ]
-      const addItems = await client.checkout.addLineItems(
-        newCheckout.id,
-        lineItems
-      )
+      const addItems = await client.checkout.addLineItems(checkoutId, lineItems)
       // Buy Now Button Code
       // window.open(addItems.webUrl, "_blank")
+      console.log(addItems.webUrl)
     } catch (e) {
       console.error(e)
     }

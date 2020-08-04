e9a616065619fc2c4464cf107bb3d7b90d9ac4ef
Kellen Presta <kpresta04@gmail.com>
2020-08-04 07:12:35 -0500

    fixed span appearing on product with no variants

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -82,21 +82,23 @@ const ProductTemplate = ({ pageContext }) => {
                         ))}
                       </select>
                     )}
-                    <span
-                      css={tw`absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center`}
-                    >
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        css={tw`w-4 h-4`}
-                        viewBox="0 0 24 24"
+                    {product.variants.length > 1 && (
+                      <span
+                        css={tw`absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center`}
                       >
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          css={tw`w-4 h-4`}
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M6 9l6 6 6-6"></path>
+                        </svg>
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>

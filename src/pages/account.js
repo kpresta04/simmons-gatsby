3c75cfc1bbbd99c6ba854881effafebd41912ad2
Kellen Presta <kpresta04@gmail.com>
2020-08-04 07:21:20 -0500

    added protection for account page

--- src/pages/account.js
+++ src/pages/account.js
@@ -76,93 +76,107 @@ const Account = () => {
   return (
     <Layout>
       <div style={{ minHeight: "70vh" }}>
-        {selectedOrder && (
-          <OrderDetail selectOrder={selectOrder} order={selectedOrder} />
-        )}
-        {!selectedOrder && (
+        {user.token.accessToken !== null ? (
           <div>
-            <Heading>My Account</Heading>
-            <button
-              style={{
-                fontSize: "15px",
-                textAlign: "center",
-                display: "flex",
-                margin: "0 auto 3rem auto",
-              }}
-              onClick={() => {
-                user.logOutUser()
-                navigate("/")
-              }}
-            >
-              Log Out
-            </button>
-
-            {!loading && (
-              <h2
-                style={{
-                  fontSize: " 1.33333em",
-                  textTransform: "uppercase",
-                  letterSpacing: "0.1em",
-                  fontWeight: "bold",
-                  marginBottom: "17.5px",
-                }}
-              >
-                Order History
-              </h2>
+            {selectedOrder && (
+              <OrderDetail selectOrder={selectOrder} order={selectedOrder} />
             )}
-
-            {!loading && orders.length > 0 ? (
-              <table
-                style={{
-                  width: "100%",
-                  borderCollapse: "collapse",
-                  borderSpacing: "0",
-                  marginBottom: "27.5px",
-                }}
-              >
-                <thead>
-                  <tr>
-                    <th scope="col">Order</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order, index) => (
-                    <tr key={index}>
-                      <th data-label="Order" scope="row">
-                        <button
-                          onClick={() => {
-                            selectOrder(order)
-                          }}
-                        >
-                          #{order.node.orderNumber}
-                        </button>
-                      </th>
-
-                      <td data-label="Date">
-                        {moment(order.node.processedAt.slice(0, 10)).format(
-                          "MMMM Do YYYY"
-                        )}
-                      </td>
-                      <td data-label="Total">${order.node.totalPrice}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            ) : (
-              !loading && <h1>No orders found.</h1>
-            )}
-
-            <Link to="/recover">Change password</Link>
-            {loading && (
+            {!selectedOrder && (
               <div>
-                <Heading>Loading Order History</Heading>
-                <RollingBlueIcon
-                  style={{ height: "40", display: "flex", margin: "0 auto" }}
-                />
+                <Heading>My Account</Heading>
+                <button
+                  style={{
+                    fontSize: "15px",
+                    textAlign: "center",
+                    display: "flex",
+                    margin: "0 auto 3rem auto",
+                  }}
+                  onClick={() => {
+                    user.logOutUser()
+                    navigate("/")
+                  }}
+                >
+                  Log Out
+                </button>
+
+                {!loading && (
+                  <h2
+                    style={{
+                      fontSize: " 1.33333em",
+                      textTransform: "uppercase",
+                      letterSpacing: "0.1em",
+                      fontWeight: "bold",
+                      marginBottom: "17.5px",
+                    }}
+                  >
+                    Order History
+                  </h2>
+                )}
+
+                {!loading && orders.length > 0 ? (
+                  <table
+                    style={{
+                      width: "100%",
+                      borderCollapse: "collapse",
+                      borderSpacing: "0",
+                      marginBottom: "27.5px",
+                    }}
+                  >
+                    <thead>
+                      <tr>
+                        <th scope="col">Order</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Total</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders.map((order, index) => (
+                        <tr key={index}>
+                          <th data-label="Order" scope="row">
+                            <button
+                              onClick={() => {
+                                selectOrder(order)
+                              }}
+                            >
+                              #{order.node.orderNumber}
+                            </button>
+                          </th>
+
+                          <td data-label="Date">
+                            {moment(order.node.processedAt.slice(0, 10)).format(
+                              "MMMM Do YYYY"
+                            )}
+                          </td>
+                          <td data-label="Total">${order.node.totalPrice}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  !loading && <h1>No orders found.</h1>
+                )}
+
+                <Link to="/recover">Change password</Link>
+                {loading && (
+                  <div>
+                    <Heading>Loading Order History</Heading>
+                    <RollingBlueIcon
+                      style={{
+                        height: "40",
+                        display: "flex",
+                        margin: "0 auto",
+                      }}
+                    />
+                  </div>
+                )}
               </div>
             )}
+          </div>
+        ) : (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1 style={{ marginTop: "3rem", fontSize: "2rem" }}>
+              You must be logged in to view this page.
+            </h1>
           </div>
         )}
       </div>

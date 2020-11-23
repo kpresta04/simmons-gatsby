028fb4cfe0add9b3945ac1dad96bb9c987c01b74
Kellen Presta <kpresta04@gmail.com>
2020-11-23 08:41:16 -0600

    build page seo

--- src/pages/build-classes.js
+++ src/pages/build-classes.js
@@ -17,6 +17,10 @@ export const query = graphql`
 export default function BuildClasses({ data }) {
   return (
     <Layout>
+      <SEO
+        pageDesc="Come build your very own AR-15 with the guidance of our smiths! Approximately 2-hour class with social time at the end. Call now for class dates and times and to sign up for the next class!"
+        pageTitle={"AR-15 Build Classes - Simmons Gun Repair"}
+      />
       <StatsWithImage2
         imageSrc={data.buildPhoto.childImageSharp.fluid}
       ></StatsWithImage2>

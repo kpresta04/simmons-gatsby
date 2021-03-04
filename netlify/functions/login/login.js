2d8c609fc1381d4b7f166eadfa168cef110b52dd
Kellen Presta <kpresta04@gmail.com>
2021-03-04 17:34:00 -0600

    df

--- netlify/functions/login/login.js
+++ netlify/functions/login/login.js
@@ -13,24 +13,28 @@ const fetchGraphQL = query =>
   })
 
 exports.handler = async function (event, context) {
-  const logInMutation = `
-	mutation  {
-customerAccessTokenCreate(input: {
-email: "${event.body.email}",
-password: "${event.body.password}"
-}) {
-customerUserErrors {
-code
-field
-message
-}
-customerAccessToken {
-accessToken
-expiresAt
-}
-}
-}`
-  const response = await fetchGraphQL(logInMutation)
+  //   const logInMutation = `
+  // 	mutation  {
+  // customerAccessTokenCreate(input: {
+  // email: "${event.body.email}",
+  // password: "${event.body.password}"
+  // }) {
+  // customerUserErrors {
+  // code
+  // field
+  // message
+  // }
+  // customerAccessToken {
+  // accessToken
+  // expiresAt
+  // }
+  // }
+  // }`
+  //   const response = await fetchGraphQL(logInMutation)
+  const response = {
+    username: event.body.username,
+    password: event.body.password,
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(response),

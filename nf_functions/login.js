fa83918089fd22c200acdc9906f5a071426e9977
Kellen Presta <kpresta04@gmail.com>
2021-03-04 10:25:18 -0600

    te

--- nf_functions/login.js
+++ nf_functions/login.js
@@ -1,4 +1,16 @@
-require("../src/utils/fetchGraphQL")
+require("axios")
+
+const fetchGraphQL = query =>
+  axios({
+    method: "post",
+    url: `https://${process.env.GATSBY_SHOPIFY_URI}/api/graphql`,
+    headers: {
+      "X-Shopify-Storefront-Access-Token":
+        process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
+      "Content-Type": "application/graphql",
+    },
+    data: query,
+  })
 
 exports.handler = async function (event, context) {
   return {

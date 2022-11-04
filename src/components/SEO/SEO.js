e54202a012878b451cd73418bccb7f6d38b4ed62
Kellen Presta <kpresta04@gmail.com>
2022-11-04 17:54:22 +0000

    fix tyypo

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 export default function SEO({
-  pageDesc = "ons Gun Repair & Sales has been repairing firearms & putting our Simmons Floating Vent Ribs on shotguns since 1945. Call or visit today!",
+  pageDesc = "Simmons Gun Repair & Sales has been repairing firearms & putting our Simmons Floating Vent Ribs on shotguns since 1945. Call or visit today!",
   pageTitle = "Simmons Gun Repair- Olathe Gun Shop & Firearm Repair | Kansas City",
 }) {
   return (

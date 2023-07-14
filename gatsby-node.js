5321b55ac27346dc412a35dcc3a3c2cbb66efa8a
Kellen Presta <kpresta04@gmail.com>
2023-07-14 14:24:55 +0100

    update plugin versions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,58 +1,55 @@
 const path = require(`path`)
 exports.createPages = async ({ graphql, actions }) => {
-  // const { createPage } = actions
-  // // Query for all products in Shopify
-  // const result = await graphql(`
-  //   query {
-  //     products(sortKey: TITLE, first: 250) {
-  //       edges {
-  //         node {
-  //           images(first: 250) {
-  //             nodes {
-  //               url
-  //             }
-  //           }
-  //           title
-  //           id
-  //           variants(first: 250) {
-  //             nodes {
-  //               id
-  //               price {
-  //                 amount
-  //               }
-  //               title
-  //               availableForSale
-  //               selectedOptions {
-  //                 name
-  //               }
-  //             }
-  //           }
-  //           handle
-  //           descriptionHtml
-  //           description
-  //           availableForSale
-  //           priceRange {
-  //             maxVariantPrice {
-  //               amount
-  //             }
-  //             minVariantPrice {
-  //               amount
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const { createPage } = actions
+  // Query for all products in Shopify
+  const result = await graphql(`
+    query {
+      allShopifyProduct(limit: 250) {
+        edges {
+          node {
+            title
+            id
+            variants {
+              id
+              title
+              availableForSale
+              selectedOptions {
+                name
+              }
+              price
+            }
+            handle
+            descriptionHtml
+            priceRangeV2 {
+              maxVariantPrice {
+                amount
+              }
+              minVariantPrice {
+                amount
+              }
+            }
+            media {
+              ... on ShopifyMediaImage {
+                id
+                image {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
   // Iterate over all products and create a new page using a template
   // The product "handle" is generated automatically by Shopify
-  // result.data.products.edges.node.forEach(({ node }) => {
-  //   createPage({
-  //     path: `/product/${node.handle}`,
-  //     component: path.resolve(`./src/components/products/Product.js`),
-  //     context: {
-  //       product: node,
-  //     },
-  //   })
-  // })
+  result.data.allShopifyProduct.edges.node.forEach(({ node }) => {
+    createPage({
+      path: `/product/${node.handle}`,
+      component: path.resolve(`./src/components/products/Product.js`),
+      context: {
+        product: node,
+      },
+    })
+  })
 }
